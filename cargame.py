e177269cf2ef1d940e9994db2e92959c71df7671
annie-hola <anhanhanh151199@gmail.com>
2024-02-18 22:29:26 +0700

    run screen

--- cargame.py
+++ cargame.py
@@ -3,10 +3,11 @@ from pygame.locals import *
 
 pygame.init()
 running = True
+screen = pygame.display.set_mode((800,800))
 
 while running:
     for event in pygame.event.get():
-        
-        
+        if event.type == QUIT:
+            running = False     
 
 pygame.quit()
