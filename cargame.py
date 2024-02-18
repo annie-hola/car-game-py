604f81092152962057b8b3bb5bd3dad207c3f617
annie-hola <anhanhanh151199@gmail.com>
2024-02-18 22:55:59 +0700

    draw graphhics

--- cargame.py
+++ cargame.py
@@ -1,9 +1,26 @@
 import pygame
 from pygame.locals import *
 
+size = width, height = (800, 800)
+road_w = int(width/1.6)
+
 pygame.init()
 running = True
-screen = pygame.display.set_mode((800,800))
+# set window size
+screen = pygame.display.set_mode(size)
+#set window title
+pygame.display.set_caption("Car Game")
+#set color
+screen.fill((60, 220, 0))
+
+#draw graphics - road
+pygame.draw.rect(
+    screen,
+    (50,50,50),
+    (width/2- road_w/2, 0, road_w, width)
+)
+# apply changes
+pygame.display.update()
 
 while running:
     for event in pygame.event.get():
