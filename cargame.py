aa9aabd48fa44fbfe8504d6ab1e2592b5327cb9b
annie-hola <anhanhanh151199@gmail.com>
2024-02-18 23:10:30 +0700

    add car

--- cargame.py
+++ cargame.py
@@ -3,6 +3,7 @@ from pygame.locals import *
 
 size = width, height = (800, 800)
 road_w = int(width/1.6)
+roadmark_w = int(width/80)
 
 pygame.init()
 running = True
@@ -19,6 +20,12 @@ pygame.draw.rect(
     (50,50,50),
     (width/2- road_w/2, 0, road_w, width)
 )
+  # draw centre line
+pygame.draw.rect(
+    screen,
+    (255, 240, 60),
+    (width/2 - roadmark_w/2, 0, roadmark_w, height))
+
 # apply changes
 pygame.display.update()
 
